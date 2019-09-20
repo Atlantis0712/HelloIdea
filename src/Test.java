dbb0c10622f662300edc8032efda102e9217fe0c
Atlantis <atlantis_company@foxmail.com>
2019-09-20 13:08:12 +0800

    Atlantis

---
+++ src/Test.java
@@ -0,0 +1,22 @@
+import java.util.Date;
+
+/**
+ * @Auther: Atlantis
+ * @Date: 2019/9/17
+ * @Description: PACKAGE_NAME
+ * @version: 1.0
+ */
+public class Test {
+
+    /**
+     * 功能描述:
+     * @param: 
+     * @return: 
+     * @auther: Atlantis
+     * @date:  
+     */ public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Date date = new Date();
+        System.out.println(date);
+    }
+}
